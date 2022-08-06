da088f236d5ee88ac6c40710be048bf1bbe1744a
KapPristas <109632944+KapPristas@users.noreply.github.com>
2022-08-06 03:03:22 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,18 +1,15 @@
 <head>
 <?php
-#zmienne
-$cenpal = 8; //cena paliwa
-$katsam = "Standard"; //kategoria samochodu
-$lok = "Warszawa"; //lokalizacja
-$bazcen = 100; //bazowa cena za dobe
-$śrespal = 8; //średnie spalanie
-$ilomod = 2; //ilość modeli
+$petrolCost = 8; 
+$carCategory = "Standard"; 
+$localization = "Warszawa";
+$baseCost = 100; 
+$averagePetrolUsage = 8; 
+$modelsQuantity = 2; 
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <meta charset="utf-8">
     <link rel="stylesheet" href="app.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kalkulator cen</title>
-    
   </head>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pl">
 <?php require_once("header.php"); ?>
 
   <body>
@@ -11,69 +11,69 @@
             </div>
             <div class="mb-2">
                 <label for="prawo" class="form-label">Rok uzyskania prawa jazdy</label>
-                <input type="number" class="form-control" name="prawo" ></input>
+                <input type="number" class="form-control" name="license" >
             </div>
             <div class="mb-1">
                 <label for="daty" class="form-label">Od</label>
-                <input type="date" class="form-control" name="od" ></input>
+                <input type="date" class="form-control" name="from" >
                 <label for="daty" class="form-label">Do</label>
-                <input type="date" class="form-control" name="do" ></input>
+                <input type="date" class="form-control" name="to" >
             </div>
-            <input  type="submit" name="Zatwierdź" class="btn btn-primary" value="Zatwierdź"></input>
-            <input  type="submit" name="Anuluj" class="btn btn-danger" value="Anuluj"></input><br><br>
+            <input  type="submit" name="Confirm" class="btn btn-primary" value="Confirm">
+            <input  type="submit" name="Anuluj" class="btn btn-danger" value="Anuluj"><br><br>
             <?php
 
-            if(isset($_POST["Zatwierdź"])){
+            if(isset($_POST["Confirm"])){
                 if (isset($_POST['km'])) {
                     $km = $_POST['km'];
                 }
-                if (isset($_POST['prawo'])) {
-                    $prawo = $_POST['prawo'];
+                if (isset($_POST['license'])) {
+                    $license = $_POST['license'];
                 }
-                if (isset($_POST['od'])) {
-                    $od = $_POST['od'];
+                if (isset($_POST['from'])) {
+                    $from = $_POST['from'];
                 }
-                if (isset($_POST['do'])) {
-                    $do = $_POST['do'];
+                if (isset($_POST['to'])) {
+                    $to = $_POST['to'];
                 }
-                $cena = 0;
-                $dni = abs(round((strtotime($od) - strtotime($do))/86400));
-                $cena = $dni * $bazcen;
-                if($katsam == "Standard"){
-                    $cena *= 1.3;
+                $price = 0;
+                $days = abs(round((strtotime($from) - strtotime($to))/86400));
+                $price = $days * $baseCost;
+                if($carCategory == "Standard"){
+                    $price *= 1.3;
                 }
-                elseif($katsam == "Medium"){
-                    $cena *= 1.6;
+                elseif($carCategory == "Medium"){
+                    $price *= 1.6;
                 }
-                elseif($katsam == "Premium"){
-                    $cena *= 2;
+                elseif($carCategory == "Premium"){
+                    $price *= 2;
                 }
                 $date = date("Y");
-                if ($date-$prawo<=5){
-                    $cena *= 1.2;
+                if ($date-$license<=5){
+                    $price *= 1.2;
                 }
-                if ($date-$prawo<3 && $katsam == "Premium"){
-                    echo '<script>alert("Nie można wypożyczyć samochodu")</script>';
+                if ($date-$license<3 && $carCategory == "Premium"){
+                    echo '<script>alert("Nie można wypożyczyć samochodu, kategoria Premium dostępna tylko dla klientów posiadających prawo jazdy dłużej niż 3 lata.")</script>';
                 }
-                if($ilomod<3){
-                    $cena *= 1.15;
+                if($modelsQuantity<3){
+                    $price *= 1.15;
                 }
-                    $cena = $cena + (($cenpal*$śrespal)*($km/100));
+                    $price = $price + (($petrolCost*$averagePetrolUsage)*($km/100));
                 ?>
                Cena netto:  <input type="text" name="txt" value="<?php
-                 echo round($cena, 2);
+                 echo round($price, 2);
                 ?>" disabled>
                 Cena brutto:  <input type="text" name="txt" value="<?php
-                 echo round($cena*1.23, 2);
+                 echo round($price*1.23, 2);
                 ?>" disabled> <br><br>
                 <?php
-                echo "W tym cena paliwa: " . (($cenpal*$śrespal)*($km/100)) . "zł" . "<br>";
-                echo "Wynajem na " . $dni . " dni: " . $dni * $bazcen . "zł" .  "<br>";
-                echo "Cena za dzień wypożyczenia wynosi: " . $bazcen . "zł" .  "<br>";  
-                if ($date-$prawo<=5){
+                echo "W tym cena paliwa: " . (($petrolCost*$averagePetrolUsage)*($km/100)) . "zł" . "<br>";
+                echo "Wynajem na " . $days . " dni: " . $days * $baseCost . "zł" .  "<br>";
+                echo "Cena za dzień wypożyczenia wynosi: " . $baseCost . "zł" .  "<br>";  
+                if ($date-$license<=5){
                     echo "Informujemy także że z powodu posiadania prawa jazdy nie dłużej niż 5 lat rachunek został podniesiony o 20%".  "<br>";
                 }          
-                if($ilomod<3){
+                if($modelsQuantity<3){
                     echo "Informujemy także że z powodu małej ilości wybranych modeli rachunek został podniesiony o 15%".  "<br>";
                 }
             }
@@ -81,6 +81,5 @@
             ?>
     </form>
 
-  </body>
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </html>
